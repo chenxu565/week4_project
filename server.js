91fb4053e05cc2489cec7327a755c708354f0bb6
Erno Vanhala <erno.vanhala@lut.fi>
2021-09-28 16:48:53 +0300

    Das code #2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const app = express();
 const port = 8000;
 
-//app.use(express.json());
+app.use(express.json());
 //app.use(express.urlencoded({extended: false}));
 
 
